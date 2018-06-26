f8f0fab17faf10e8c53fb054456ac9fd96cf375a
maskleo <masque@aliyun.com>
2018-06-26 10:13:53 +0800

    SpringBoot Admin Demo init

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ spring-boot-admin demo
 http://codecentric.github.io/spring-boot-admin/2.0.1/#_ui
 
 http://www.cnblogs.com/ityouknow/p/8440455.html
+
+
+https://blog.csdn.net/clementad/article/details/70613209
--- bootadmin/src/main/java/edu/maskleo/bootadmin/BootadminApplication.java
+++ bootadmin/src/main/java/edu/maskleo/bootadmin/BootadminApplication.java
@@ -1,12 +1,15 @@
 package edu.maskleo.bootadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class BootadminApplication {
 
     public static void main(String[] args) {
