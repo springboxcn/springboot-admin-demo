0301afc1a50178ec4955fc4525a18d7af70ddefc
maskleo <masque@aliyun.com>
2018-06-26 15:20:12 +0800

    添加依赖结构

---
+++ admin-client/src/main/java/edu/maskleo/adminclient/TestController.java
@@ -0,0 +1,15 @@
+package edu.maskleo.adminclient;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> index(){
+        return ResponseEntity.ok("hello");
+    }
+
+}
